b3befa0261a6b87371d326d250ea81a3ca8557b5
cse <cse@ableton.com>
2014-06-09 13:44:52 +0200

    Add thumbnail to file items

--- FlatCommander/js/default.js
+++ FlatCommander/js/default.js
@@ -22,6 +22,9 @@
                 var itemQuery = storageFolder.createItemQuery();
 
                 var dataSourceOptions = {
+                    mode: Windows.Storage.FileProperties.ThumbnailMode.listView,
+                    requestedThumbnailSize: 32,
+                    thumbnailOptions: Windows.Storage.FileProperties.ThumbnailOptions.none
                 };
 
                 var dataSource = new WinJS.UI.StorageDataSource(itemQuery, dataSourceOptions);
@@ -49,13 +52,24 @@
             element = document.createElement("div");
         }
 
+        var itemNameElement = document.createElement("span");
+        var itemThumbnailElement = document.createElement("img");
+
+        element.appendChild(itemThumbnailElement);
+        element.appendChild(itemNameElement);
+
         return {
             // returns the placeholder
             element: element,
             // and a promise that will complete when the item is fully rendered
             renderComplete: itemPromise.then(function (item) {
-                element.textContent = item.data.displayName;
+                itemNameElement.textContent = item.data.displayName;
                 return item.ready;
+            }).then(function (item) {
+                // wait until item.ready before doing expensive work
+                return WinJS.UI.StorageDataSource.loadThumbnail(item, itemThumbnailElement).then(function (image) {
+                    // perform any operation that requires the thumbnail to be available
+                });
             })
         };
     }
