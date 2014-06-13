0a97bd5cc5454a3d6ade81e6c89b23d5fb7a3456
cse <cse@ableton.com>
2014-06-13 17:12:07 +0200

    Use a template for rendering storage items
    We use the WinJS.Binding.Template method for templating the storage items.
    This is a little tricky, since most information about the items (display
    name, but also thumbnail, etc) are only available asynchronously, so the
    we still need a bit of code for binding converters which register event
    handlers that listen to updates to the items' properties.
    
    The bindThumbnail() function comes directly from a VS sample
    ("StorageDataSource and GetVirtualizedFilesVector sample"), the
    bindStorageItemProperty() function is homegrown.

--- FlatCommander/js/FolderPanel.js
+++ FlatCommander/js/FolderPanel.js
@@ -7,35 +7,6 @@
     });
 
 
-    function storageRenderer(itemPromise, element) {
-        if (!element) {
-            // dom is not recycled, so create inital structure
-            element = document.createElement("div");
-        }
-
-        var itemNameElement = document.createElement("span");
-        var itemThumbnailElement = document.createElement("img");
-
-        element.appendChild(itemThumbnailElement);
-        element.appendChild(itemNameElement);
-
-        return {
-            // returns the placeholder
-            element: element,
-            // and a promise that will complete when the item is fully rendered
-            renderComplete: itemPromise.then(function (item) {
-                itemNameElement.textContent = item.data.displayName;
-                return item.ready;
-            }).then(function (item) {
-                // wait until item.ready before doing expensive work
-                return WinJS.UI.StorageDataSource.loadThumbnail(item, itemThumbnailElement).then(function (image) {
-                    // perform any operation that requires the thumbnail to be available
-                });
-            })
-        }
-    };
-
-
     function goToFolder(storageFolder) {
         this._saveCurrentItem();
 
@@ -107,7 +78,7 @@
         var listViewElement = folderPanelContainer.querySelector("#folderItems");
 
         var listViewOptions = {
-            itemTemplate: storageRenderer,
+            itemTemplate: folderItemTemplate,
             layout: new WinJS.UI.ListLayout(),
             selectionMode: "single"
         };
--- FlatCommander/js/Utilities.js
+++ FlatCommander/js/Utilities.js
@@ -1,5 +1,73 @@
 ﻿(function () {
     "use strict";
 
-    WinJS.Namespace.define("FlatCommander.Utilities", {});
+    function bindThumbnail(source, sourceProperty, destination, destinationProperty) {
+        var image = destination;
+        var item = source;
+
+        var thumbnailUpdateHandler,
+                firstImage = true,
+                shouldRespondToThumbnailUpdate = false;
+
+        // Load a thumbnail if it exists.
+        var processThumbnail = function (thumbnail) {
+            if (thumbnail) {
+                var url = URL.createObjectURL(thumbnail, { oneTimeOnly: true });
+
+                // If this is the first version of the thumbnail we're loading, fade it in.
+                if (firstImage) {
+                    image.onload = function () {
+                        WinJS.UI.Animation.fadeIn(image);
+                    };
+                    firstImage = false;
+                } else {
+                    image.onload = null;
+                }
+                image.src = url;
+
+                // If we have the full resolution thumbnail, we can cancel further updates and complete the promise
+                // when current work is complete.
+                if ((thumbnail.type !== Windows.Storage.FileProperties.ThumbnailType.icon) && !thumbnail.returnedSmallerCachedSize) {
+                    item.removeEventListener("thumbnailupdated", thumbnailUpdateHandler);
+                    shouldRespondToThumbnailUpdate = false;
+                }
+            }
+        };
+
+        thumbnailUpdateHandler = function (e) {
+            // Ensure that a zombie update handler does not get invoked.
+            if (shouldRespondToThumbnailUpdate) {
+                processThumbnail(e.target.thumbnail);
+            }
+        };
+        item.addEventListener("thumbnailupdated", thumbnailUpdateHandler);
+        shouldRespondToThumbnailUpdate = true;
+
+        // If we already have a thumbnail we should render it now.
+        processThumbnail(item.thumbnail);
+    }
+
+    WinJS.Utilities.markSupportedForProcessing(bindThumbnail);
+
+
+    function bindStorageItemProperty(storageItem, property, element, attribute) {
+        var updateHandler = function (event) {
+            for (var i = 0; i < property.length; i++) {
+                var propVal = storageItem[property[i]];
+                if (propVal) {
+                    element[attribute[i]] = storageItem[property[i]];
+                }
+            }
+        };
+        storageItem.addEventListener("propertiesupdated", updateHandler, false);
+        updateHandler(null);
+    }
+
+    WinJS.Utilities.markSupportedForProcessing(bindStorageItemProperty);
+
+
+    WinJS.Namespace.define("FlatCommander.Utilities", {
+        bindThumbnail: bindThumbnail,
+        bindStorageItemProperty: bindStorageItemProperty
+    });
 })();
